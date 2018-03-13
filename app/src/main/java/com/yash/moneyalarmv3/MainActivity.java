2f48007f657065d9e301f3b4a5af99483192e3ed
staryash@github.com <staryash@gmail.com>
2018-03-13 16:52:14 +0530

    intial commit

---
+++ app/src/main/java/com/yash/moneyalarmv3/MainActivity.java
@@ -0,0 +1,51 @@
+package com.yash.moneyalarmv3;
+
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    SharedPreferences alarmPref;
+    SharedPreferences.Editor prefEditor;
+    TextView tokenNoView;
+    //problem
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        alarmPref = this.getPreferences(this.MODE_PRIVATE);
+
+        tokenNoView = (TextView)findViewById(R.id.tokenNoDisplay);
+
+        tokenNoView.setText(String.valueOf(currentToken()));
+    }
+
+    void increaseToken(int inc) {
+        int tokenNumber = inc + currentToken();
+        setToken(tokenNumber);
+
+    }
+
+    void consumeToken() {
+        int tokenNumber = currentToken() -1;
+        setToken(tokenNumber);
+
+    }
+
+    int currentToken() {
+        return alarmPref.getInt("tokenNo", 5);
+    }
+
+    void setToken(int tokenNumber){
+        prefEditor = alarmPref.edit();
+        prefEditor.putInt("tokenNo",tokenNumber);
+        prefEditor.apply();
+        TextView tokenNoView = (TextView) findViewById(R.id.tokenNoDisplay);
+        tokenNoView.setText(String.valueOf(currentToken()));
+    }
+
+
+}
