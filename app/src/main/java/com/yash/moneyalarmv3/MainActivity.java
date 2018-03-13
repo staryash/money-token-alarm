77bf44f5e84f08218f9987e009f573bd3eb396f1
Yash Sharma <yaashg@gmail.com>
2018-03-13 20:15:33 +0530

    for dinner

--- app/src/main/java/com/yash/moneyalarmv3/MainActivity.java
+++ app/src/main/java/com/yash/moneyalarmv3/MainActivity.java
@@ -1,15 +1,22 @@
 package com.yash.moneyalarmv3;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     SharedPreferences alarmPref;
     SharedPreferences.Editor prefEditor;
     TextView tokenNoView;
+    private static TextView alarmMessageBox;
     //problem hai?
 
     @Override
@@ -19,23 +26,23 @@ public class MainActivity extends AppCompatActivity {
         alarmPref = this.getPreferences(this.MODE_PRIVATE);
 
         tokenNoView = (TextView)findViewById(R.id.tokenNoDisplay);
-
-        tokenNoView.setText(String.valueOf(currentToken()));
+        tokenNoView.setText(String.valueOf(getToken()));
+        alarmMessageBox =(TextView)findViewById(R.id.alarmMessageBox);
     }
 
-    void increaseToken(int inc) {
-        int tokenNumber = inc + currentToken();
+    void increaseToken(View view) {
+        int tokenNumber = 5 + getToken();
         setToken(tokenNumber);
 
     }
 
-    void consumeToken() {
-        int tokenNumber = currentToken() -1;
+    void consumeToken(View view) {
+        int tokenNumber = getToken() -1;
         setToken(tokenNumber);
 
     }
 
-    int currentToken() {
+    int getToken() {
         return alarmPref.getInt("tokenNo", 5);
     }
 
@@ -44,7 +51,30 @@ public class MainActivity extends AppCompatActivity {
         prefEditor.putInt("tokenNo",tokenNumber);
         prefEditor.apply();
         TextView tokenNoView = (TextView) findViewById(R.id.tokenNoDisplay);
-        tokenNoView.setText(String.valueOf(currentToken()));
+        tokenNoView.setText(String.valueOf(getToken()));
+    }
+
+    void createAlarm(View view){
+        EditText hrsInput = (EditText)findViewById(R.id.hrsInput);
+        EditText minInput = (EditText)findViewById(R.id.minInput);
+        int hrs = Integer.parseInt(hrsInput.getText().toString());
+        int min = Integer.parseInt(minInput.getText().toString());
+
+        Toast.makeText(this,"creating alarm for "+hrs+":"+min,Toast.LENGTH_LONG).show();
+
+        //creating alarm below
+        AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+        Intent myIntent = new Intent(MainActivity.this, AlarmReceiver.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(MainActivity.this, 0, myIntent, 0);
+        alarmManager.setExact(AlarmManager.RTC_WAKEUP, 0, pendingIntent);
+
+    }
+    public static TextView alarmMessage(){
+        return alarmMessageBox;
+    }
+
+    void deleteAlarm(View view){
+        
     }
 
 
